405307656ea5ac9fa657fcd37ee7def56471e285
Maxim Akulin <maxim.akulin@gmail.com>
2017-11-26 00:03:22 +0500

    done

--- async.js
+++ async.js
@@ -3,11 +3,36 @@
 exports.isStar = true;
 exports.runParallel = runParallel;
 
-/** Функция паралелльно запускает указанное число промисов
- * @param {Array} jobs – функции, которые возвращают промисы
- * @param {Number} parallelNum - число одновременно исполняющихся промисов
- * @param {Number} timeout - таймаут работы промиса
- */
-function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+function runParallel(sourseJobs, parallelNum, timeout = 1000) {
+    return new Promise(resolve => {
+        if (parallelNum <= 0 || !sourseJobs.length) {
+            resolve([]);
+        }
+        let jobs = sourseJobs.map(job =>
+            () => new Promise((_resolve, _reject) => {
+                job().then(_resolve, _reject);
+                setTimeout(() => _reject(new Error('Promise timeout')), timeout);
+            })
+        );
+        let startCount = 0;
+        let finishCount = 0;
+        let result = [];
+        let startJob = job => {
+            let index = startCount++;
+            let resultHendler = jobResult => finishJob(jobResult, index);
+            job().then(resultHendler)
+                .catch(resultHendler);
+        };
+        function finishJob(jobResult, index) {
+            result[index] = jobResult;
+            if (jobs.length === ++finishCount) {
+                resolve(result);
+            }
+            if (startCount < jobs.length) {
+                startJob(jobs[startCount]);
+            }
+        }
+        jobs.slice(0, parallelNum).forEach(job => startJob(job));
+    });
 }
+
